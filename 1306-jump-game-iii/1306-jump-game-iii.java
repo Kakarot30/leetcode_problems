bee7a5f52190693b07c22f4c9bc242effef57cc4
Nikunj Tyagi <nikunj2430@gmail.com>
2026-05-17 19:37:27 +0530

    Time: 1 ms (99.73%), Space: 52.2 MB (51.59%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    private boolean dfs(int[] arr, int i){
+        if(i<0||i>=arr.length||arr[i]<0){
+            return false;
+        }
+        if(arr[i]==0){
+            return true;
+        }
+        arr[i]*=-1;
+        boolean left = dfs(arr,i-arr[i]);
+        boolean right = dfs(arr,i+arr[i]);
+        return left||right;
+    }
+    public boolean canReach(int[] arr, int start) {
+        return dfs(arr,start);
+    }
+}

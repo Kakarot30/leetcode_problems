2adf7fbe456ec35d31272b76d24a7eb45a5bde22
Nikunj Tyagi <nikunj2430@gmail.com>
2026-06-06 11:43:18 +0530

    Time: 2 ms (98.84%), Space: 46.5 MB (68.25%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
+        // int n = nums.length;
+        // int[] rightSum = new int[n];
+        // int[] leftSum = new int[n];
+        // leftSum[0]=0;
+        // for(int i=1;i<n;i++){
+        //     leftSum[i]=leftSum[i-1]+nums[i-1];
+        // }
+        // rightSum[n-1]=0;
+        // for(int i=n-2;i>=0;i--){
+        //     rightSum[i]=rightSum[i+1]+nums[i+1];
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=Math.abs(leftSum[i]-rightSum[i]);
+        // }
+        // return nums;
+        int sum =0;
         int n = nums.length;
-        int[] rightSum = new int[n];
-        int[] leftSum = new int[n];
-        leftSum[0]=0;
-        for(int i=1;i<n;i++){
-            leftSum[i]=leftSum[i-1]+nums[i-1];
-        }
-        rightSum[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            rightSum[i]=rightSum[i+1]+nums[i+1];
-        }
         for(int i=0;i<n;i++){
-            nums[i]=Math.abs(leftSum[i]-rightSum[i]);
+            sum = sum+nums[i];
         }
-        return nums;
+        int[] result = new int[n];
+        int curr= 0;
+        for(int i =0;i<n;i++){
+            int leftSum = curr;
+            curr+=nums[i];
+            int rightSum = sum-curr;
+            result[i]= Math.abs(leftSum-rightSum);
+        }
+        return result;
     }
 }

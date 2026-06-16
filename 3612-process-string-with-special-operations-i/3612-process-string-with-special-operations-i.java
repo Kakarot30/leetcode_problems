79545415242765ba1488e64db92004a7dff5d9ec
Nikunj Tyagi <nikunj2430@gmail.com>
2026-06-16 12:17:17 +0530

    Time: 4 ms (76.47%), Space: 55.7 MB (20.59%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,32 +1,19 @@
 class Solution {
     public String processStr(String s) {
-        int n = s.length();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (Character.isLowerCase(s.charAt(i))) {
-                sb.append(s.charAt(i));
-            } else if (s.charAt(i) == '#') {
-                sb = new StringBuilder(duplicate(sb.toString()));
-            } else if (s.charAt(i) == '%') {
-                sb = new StringBuilder(reverse(sb.toString()));
-            } else if (sb.length() > 0) {
-                sb.deleteCharAt(sb.length() - 1);
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLowerCase(ch)){
+                sb.append(ch);
+            }else if( ch == '#'){
+                sb.append(sb.toString());
+            }else if(ch == '%'){
+                sb.reverse();
+            }else  { 
+                if(sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
             }
-        }
-        return sb.toString();
-    }
-
-    private String duplicate(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.append(s);
-        return sb.toString();
-    }
-
-    private String reverse(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.insert(0, s.charAt(i));
         }
         return sb.toString();
     }

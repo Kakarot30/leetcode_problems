7d941b27269f8ac047daeb2fc530c3403cf403be
Nikunj Tyagi <nikunj2430@gmail.com>
2026-06-19 19:16:25 +0530

    Time: 371 ms (24.9%), Space: 42.4 MB (93.17%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -3,24 +3,24 @@ class Solution {
     int n ;
     public int distributeCookies(int[] cookies, int k) {
         n = cookies.length;
-        int[] child = new int[k];
-        solve(0,child,cookies,k);
+        int[] children = new int[k];
+        solve(0,children,cookies,k);
         return result;
      }
-    private void solve(int idx, int[] child, int[] cookies, int k){
+    private void solve(int idx, int[] children, int[] cookies, int k){
         if(idx==n){
             int ans = 0;
-            for(int childs: child){
-                ans= Math.max(ans,childs);
+            for(int child: children){
+                ans= Math.max(ans,child);
             }
             result = Math.min(ans,result);
             return;
         }
         int candy = cookies[idx];
         for(int i=0;i<k;i++){
-            child[i]+=candy;
-            solve(idx+1,child,cookies,k);
-            child[i]-=candy;
+            children[i]+=candy;
+            solve(idx+1,children,cookies,k);
+            children[i]-=candy;
         }
     }
 }
